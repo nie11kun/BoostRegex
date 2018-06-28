b10806b2e2faea7a308c424020c0bc2be8d1fb1d
Marco Nie <nie11kun@outlook.com>
2018-06-28 23:59:59 +0800

    init

--- TestRegex/main.cpp
+++ TestRegex/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+#include "boost/regex.hpp"
+#include "string"
+
+using namespace std;
+
+
+void findAndReplaceInString(string in, char* find, char* replace)
+{
+    boost::regex e;
+    e.assign(find);
+    std::ostringstream t(std::ios::out | std::ios::binary);
+    std::ostream_iterator<char, char> oi(t);
+    boost::regex_replace(oi, in.begin(), in.end(),
+                         e, replace, boost::match_default | boost::format_all);
+    cout << t.str() <<endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    string in = "abc.de..def....def...";
+    char* find =
+    "(abc.*(?!.*(def)).*def)"
+    ;
+    char* replace =
+    "(;)"
+    ;
+    
+    findAndReplaceInString(in, find, replace);
+    
     return 0;
 }
