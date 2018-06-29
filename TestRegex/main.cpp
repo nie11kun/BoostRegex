2b79425df737f95ebda72dd0abcb4a23b6cf99dc
Marco Nie <nie11kun@outlook.com>
2018-06-29 11:40:45 +0800

    test

--- TestRegex/main.cpp
+++ TestRegex/main.cpp
@@ -1,40 +1,44 @@
-//
-//  main.cpp
-//  TestRegex
-//
-//  Created by Marco Nie on 2018/6/28.
-//  Copyright © 2018 Marco Nie. All rights reserved.
+// BoostRegex.cpp : Defines the entry point for the console application.
 //
 
+#include "stdafx.h"
 #include <iostream>
 #include "boost/regex.hpp"
 #include "string"
+#include "sstream"
 
 using namespace std;
 
 
 void findAndReplaceInString(string in, char* find, char* replace)
 {
-    boost::regex e;
-    e.assign(find);
-    std::ostringstream t(std::ios::out | std::ios::binary);
-    std::ostream_iterator<char, char> oi(t);
-    boost::regex_replace(oi, in.begin(), in.end(),
-                         e, replace, boost::match_default | boost::format_all);
-    cout << t.str() <<endl;
+	boost::regex e;
+	e.assign(find);
+	std::ostringstream t(std::ios::out | std::ios::binary);
+	std::ostream_iterator<char, char> oi(t);
+	boost::regex_replace(oi, in.begin(), in.end(),
+		e, replace, boost::match_default | boost::format_all);
+	cout << t.str() << endl;
 }
 
 int main(int argc, const char * argv[]) {
-    
-    string in = "abc.de..def....def...";
-    char* find =
-    "(abc.*(?!.*(def)).*def)"
-    ;
-    char* replace =
-    "(;)"
-    ;
-    
-    findAndReplaceInString(in, find, replace);
-    
-    return 0;
+
+	string in = "abc.def..ab....abc...def..";
+
+	char* find = (char*)
+		"((abc).*?(def))|"
+		"((ab)\\b)"
+		;
+	char* replace = (char*)
+		"(;)"
+		;
+
+	findAndReplaceInString(in, find, replace);
+
+	cin.clear();
+	cin.get();
+
+	return 0;
 }
+
+
